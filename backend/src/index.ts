16c1dcb7385d4df4a7bd55d165d823dc5632ff76
AbiyuTolosa <abiyunigussie7@gmail.com>
2025-03-30 21:04:04 +0300

    cors policy added.

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -1,14 +1,20 @@
 import express from 'express';
 import dotenv from 'dotenv'
+import cors from 'cors';
 dotenv.config();
 
+
 const app = express();
-const port = process.env.PORT;
+const PORT = process.env.PORT;
+
+
+app.use(cors({origin: 'http://localhost:3000'}));
+app.use(express.json());
 
 app.get('/', (req, res) => {
   res.send('Hello, TypeScript with Express!');
 });
 
-app.listen(port, () => {
-  console.log(`Server is running on http://localhost:${port}`);
+app.listen(PORT, () => {
+  console.log(`Server is running on http://localhost:${PORT}`);
 });
