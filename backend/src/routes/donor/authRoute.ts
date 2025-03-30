e478657b1d76c6d7576ec4ad7fb0e5729ebde71f
AbiyuTolosa <abiyunigussie7@gmail.com>
2025-03-31 00:08:26 +0300

    feat: Add donor login service, controller and route

--- backend/src/routes/donor/authRoute.ts
+++ backend/src/routes/donor/authRoute.ts
@@ -1,12 +1,12 @@
 import express from 'express'
-import { register,verifyEmail } from "../../controllers/donor/authController";
+import { login, register,verifyEmail } from "../../controllers/donor/authController";
 
 const router = express.Router();
 
 
 router.post('/register', register);
 router.get('/verify-email',  verifyEmail)
-
+router.post('/login', login)
 
 
 export const donorAuthRoutes = router;
