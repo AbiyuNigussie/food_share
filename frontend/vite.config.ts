825fde6c53f76354154284fe9aeaad806ac8eb20
AbiyuTolosa <abiyunigussie7@gmail.com>
2025-07-16 16:02:54 +0300

    hotfix: vercel file removed.

--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -1,23 +1,19 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
-import tailwindcss from '@tailwindcss/vite';
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
+import tailwindcss from "@tailwindcss/vite";
 
 // https://vite.dev/config/
 export default defineConfig({
-  plugins: [
-    react(),
-    tailwindcss()
-  ],
+  plugins: [react(), tailwindcss()],
+
+  base: process.env.VITE_BASE_PATH || "/food_share/frontend/",
 
   server: {
-  proxy: {
-    '/api': {
-      target: 'http://localhost:5000',
-      changeOrigin: true,
-    }
-  }
-}
-
-})
-
-
+    proxy: {
+      "/api": {
+        target: "http://localhost:5000",
+        changeOrigin: true,
+      },
+    },
+  },
+});
